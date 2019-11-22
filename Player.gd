62e40a1b145444f8b32ef6c95e309d090feb07bc
itlbv <artem.v.fokin@gmail.com>
2019-11-23 00:55:05 +0100

    Change Player's movement to move_and_slide, rearrange code

--- Player.gd
+++ Player.gd
@@ -1,21 +1,36 @@
 extends KinematicBody2D
 
-const SPEED = 3
+const SPEED = 250
 var velocity =  Vector2()
 
 # direction of last movement for choosing animation. right - false; left - true
 var last_direction = false;
 
+onready var animation = $AnimationPlayer
+onready var sprite = $Sprite
+
 func _ready():
 	pass
-	#screen_size = get_viewport_rect().size
+
+func _process(delta):
+	_set_animation()
+
+func _set_animation():
+	if velocity.length() > 0:
+		animation.play("walk")
+	else:
+		animation.stop()
+	
+	# flipping animation according to the last direction
+	if velocity.x != 0:
+		last_direction = velocity.x < 0
+	sprite.flip_h = last_direction
 
 func _physics_process(delta):
-	_set_velocity()
-	_set_animation()
-	move_and_collide(velocity)
+	_get_input()
+	move_and_slide(velocity)
 
-func _set_velocity():
+func _get_input():
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -1
 	elif Input.is_action_pressed("ui_right"):
@@ -29,15 +44,4 @@ func _set_velocity():
 	else: velocity.y = 0
 	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED 
-
-func _set_animation():
-	if velocity.length() > 0:
-		$AnimationPlayer.play("walk")
-	else:
-		$AnimationPlayer.stop()
-	
-	# flipping animation according to the last direction
-	if velocity.x != 0:
-		last_direction = velocity.x < 0
-	$Sprite.flip_h = last_direction
+		velocity = velocity.normalized() * SPEED
