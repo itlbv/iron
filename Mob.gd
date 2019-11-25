c64afaf362d805a2f3990861100f6e66b7a9e6aa
itlbv <artem.v.fokin@gmail.com>
2019-11-25 12:49:00 +0100

    Add path following to Mob, change its sprite position slightly

--- Mob.gd
+++ Mob.gd
@@ -1,23 +1,38 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const SPEED = 50
+var path = []
 
-# Called when the node enters the scene tree for the first time.
+onready var animation = $AnimationPlayer
+
 func _ready():
-	pass # Replace with function body.
+	animation.play("idle") 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _physics_process(delta):
+	_movePath(delta)
+
+func _movePath(delta):
+	if path.size() == 0:
+		return
+	
+	var start_point = position
+	var distance = SPEED * delta
+	for i in range(path.size()):
+		var dist_to_next = start_point.distance_to(path[0])
+		if distance <= dist_to_next and distance >= 0.0:
+			position = start_point.linear_interpolate(path[0], distance / dist_to_next)
+			break
+		distance -= dist_to_next
+		start_point = path[0]
+		path.remove(0)
 
 func _on_MeleeRange_body_entered(body):
 	if body.get_name() == self.get_name():
 		return 
-	$AnimationPlayer.play("attack")
-
+	animation.play("attack")
 
 func _on_MeleeRange_body_exited(body):
-	$AnimationPlayer.play("idle")
+	animation.play("idle")
