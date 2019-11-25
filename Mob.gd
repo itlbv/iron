1171d8bc5b9be1fd84d71cfc4fc7a967bc145224
itlbv <artem.v.fokin@gmail.com>
2019-11-25 15:12:46 +0100

    Refactor Mob for readability

--- Mob.gd
+++ Mob.gd
@@ -4,6 +4,7 @@ const SPEED = 50
 var path = []
 
 onready var animation = $AnimationPlayer
+onready var state_machine = $AnimationTree.get("parameters/playback")
 
 func _ready():
 	animation.play("idle") 
@@ -32,7 +33,9 @@ func _movePath(delta):
 func _on_MeleeRange_body_entered(body):
 	if body.get_name() == self.get_name():
 		return 
-	animation.play("attack")
+	#animation.play("attack")
+	state_machine.travel("die")
+	set_physics_process(false)
 
 func _on_MeleeRange_body_exited(body):
 	animation.play("idle")
