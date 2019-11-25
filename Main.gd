8b6bdc3c9d5ed4072a17ab36b9c5e82bdf9281b3
itlbv <artem.v.fokin@gmail.com>
2019-11-25 15:12:03 +0100

    Connect 'clicked' signal, add Mob selection

--- Main.gd
+++ Main.gd
@@ -3,8 +3,13 @@ extends Node2D
 onready var nav2d : Navigation2D = $NavMap
 onready var mob : KinematicBody2D = $Mob
 
+var selectedMob : KinematicBody2D
+
 func _ready():
-	pass 
+	get_parent().get_node("Main/Mob/Selecting").connect("clicked", self, "_on_Mob_clicked")
+
+func _on_Mob_clicked(clickedMob):
+	selectedMob = clickedMob
 
 #func _process(delta):
 #	pass
@@ -14,6 +19,8 @@ func _unhandled_input(event):
 		return
 	if event.button_index != BUTTON_RIGHT or not event.pressed:
 		return
-	
-	var new_path = nav2d.get_simple_path(mob.global_position, event.global_position, true)
-	mob.path = new_path
+	if selectedMob == null:
+		return
+
+	var new_path = nav2d.get_simple_path(selectedMob.global_position, event.global_position, true)
+	selectedMob.path = new_path
